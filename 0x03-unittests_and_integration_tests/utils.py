3eaf2112de957e6441bcaecd2aefb74ae9c8e1a9
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-11-16 12:08:37 +0100

    correction on utils.py file

--- 0x03-unittests_and_integration_tests/utils.py
+++ 0x03-unittests_and_integration_tests/utils.py
@@ -10,7 +10,7 @@ It is designed to raise clear KeyError exceptions when invalid keys are accessed
 from typing import Any, Mapping, Sequence
 
 
-def access_nested_map(nested_map: Mapping[str, Any], path: Sequence[str]) -> Any:
+def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
     """
     Retrieve a value from a nested dictionary following the provided path.
 
@@ -28,6 +28,19 @@ def access_nested_map(nested_map: Mapping[str, Any], path: Sequence[str]) -> Any
         >>> access_nested_map({"a": {"b": 2}}, ("a", "b"))
         2
     """
+    """
     for key in path:
         nested_map = nested_map[key]
     return nested_map
+    """
+
+    current = nested_map
+    for key in path:
+        if not isinstance(current, Mapping):
+            raise KeyError(key)
+        if key not in current:
+            raise KeyError(key)
+        current = current[key]
+    return current
+
+
